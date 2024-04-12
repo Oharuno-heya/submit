90799ece4b2df0d49d0d1dbc2ecde3ac77cd602e
Haruki <117291500+Oharuno-heya@users.noreply.github.com>
2024-04-12 23:40:05 +0900

    Update Result.java

--- Result.java
+++ Result.java
@@ -20,15 +20,27 @@ class Calculation {
  	 * 2つの整数を演算子で計算した結果。
    	 */
 	int res = 0;
+	/**
+ 	 * 入力された数値１を変数num1に代入するメソッド。
+   	 */
 	void number1(int pnum1) {
 		num1 = pnum1;
 	}
+	/**
+ 	 * 入力された数値2を変数num2に代入するメソッド。
+	 */
 	void number2(int pnum2) {
 		num2 = pnum2;
 	}
+	/**
+ 	 * 入力された演算子を変数opeに代入するメソッド。
+   	 */
 	void operant(String pope) {
 		ope = pope;
 	}
+	/**
+ 	 * 入力された演算子の種類の応じて計算結果を変数resに代入するメソッド。
+   	 */
 	void calc() {
 		if(ope.equals("+")) {
 			res = num1 + num2;
@@ -40,6 +52,9 @@ class Calculation {
 			res = num1 / num2;
 		}
 	}
+	/**
+ 	 * 変数resをメインクラスに返すメソッド。
+	 */
 	int getRes() {
 		return res;
 	}
