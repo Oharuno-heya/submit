f83b1d994774919ecb693c279ffe9567dcc54250
oharunoheya <haru1222.bass@gmail.com>
2024-04-14 01:48:24 +0900

    新規

---
+++ javaskillcheck6classfunction/src/javaskillcheck6classfunction/addressList.java
@@ -0,0 +1,40 @@
+package javaskillcheck6classfunction;
+
+class List{
+	private String name;
+	private Setter age;
+	private String address;
+	private String tell;
+
+	private List(){
+
+	}
+
+}
+public class addressList {
+
+	public static void main(String[] args) {
+		List list1;
+		list1 = new List();
+
+
+
+
+		/**
+		 * 以下条件を満たしたクラスを作成し、指定した文言を出力してください。
+
+			①条件：インスタンス変数にはprivateを指定してください。✅
+			②条件：コンストラクタで初期化可能としてください。
+			③条件：名前、年齢、住所、電話番号はインスタンス変数の値を出力してください。
+			④条件：年齢はStringでもintでも設定可能なSetterを用意してください。
+			⑤条件：インスタンスは複数newし、Listをループして出力してください。
+
+			出力例） 私の名前は山田です。年齢は26歳です。住所は東京大田区大森です。電話番号は090-0000-0000です。
+			私の名前は鈴木です。年齢は33歳です。住所は和歌山県和歌山市です。電話番号は090-1111-2222です。
+			私の名前は田中です。年齢は41歳です。住所は宮城県仙台市です。電話番号は090-3333-4444です。
+		 */
+
+
+	}
+
+}
