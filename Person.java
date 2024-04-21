b70f17ce68b588208a2bd1f7326326028040be97
Haruki <117291500+Oharuno-heya@users.noreply.github.com>
2024-04-21 16:09:43 +0800

    Person.java を更新

--- Person.java
+++ Person.java
@@ -4,13 +4,13 @@ package jaavaskillcheckclassfunction;
  * 6章　実力確認問題　6‐2
  */
 class Person {
-	/* 名前の変数**/
+	/*※ 名前の変数*/
 	private String name;
-	/* 年齢の変数（String型）**/
+	/** 年齢の変数（String型）*/
 	private	String ages;
-	/* 名前の変数（int型）**/
+	/** 名前の変数（int型）*/
 	private	int age;
-	/* 住所の変数**/
+	/** 住所の変数 */
 	private	String address;
 	/** 電話番号の変数**/
 	private	String tel;
