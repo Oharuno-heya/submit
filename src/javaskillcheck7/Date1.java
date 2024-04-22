9e97e2ab79ed3cce498013973e4a3b5fc32b75cb
Haruki <117291500+Oharuno-heya@users.noreply.github.com>
2024-04-23 03:01:48 +0900

    Update Date1.java

--- src/javaskillcheck7/Date1.java
+++ src/javaskillcheck7/Date1.java
@@ -1,19 +1,21 @@
 package javaskillcheck7;
+import java.time.LocalDate;
 
 /**
  * Java実力確認問題　7章　クラスの利用7‐1
  */
-import java.time.LocalDate;
-
 public class Date1 {
-	/**
-	 * 入力された日付と、1か月後の日付を出力。
-	 */
-	public static void main(String[] args){
-		// 入力された日付を出力
-		LocalDate date = LocalDate.of(2024, 05, 20);
-		System.out.println(date);
-		// 1か月後の日付を出力
-		System.out.println("1か月後は" + date.plusMonths(1) + "です。");
-	}
+    /**
+     * 入力された日付と、1か月後の日付を出力。
+     */
+    public static void main(String[] args){
+        //    入力された日付を出力
+        LocalDate date = LocalDate.of(2024, 05, 20);
+        System.out.println(date);
+
+        //    1か月後の日付を出力
+        LocalDate date2 = date.plusMonths(1);
+        int month = date2.getMonthValue();
+        System.out.println(date.getYear() + "/" + month + "/" + date.getDayOfMonth());
+    }
 }
