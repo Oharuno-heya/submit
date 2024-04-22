2a8adeecdade2f0041f079ed99280c38888daaa0
Haruki <117291500+Oharuno-heya@users.noreply.github.com>
2024-04-23 02:37:34 +0900

    Update Person.java

--- Person.java
+++ Person.java
@@ -1,68 +1,72 @@
-package jaavaskillcheckclassfunction;
+ackage jaavaskillcheckclassfunction;
 
 /**
  * 6章　実力確認問題　6‐2
  */
-public class Person {
-	/** 名前の変数 */
+class Person {
+	/**　名前の変数　*/
 	private String name;
-	/** 年齢の変数（String型）*/
+	/** 年齢の変数（String型） */
 	private	String ages;
-	/** 名前の変数（int型）*/
+	/** 名前の変数（int型） */
 	private	int age;
 	/** 住所の変数 */
 	private	String address;
 	/** 電話番号の変数 */
 	private	String tel;
-	
 	/**
 	 * 名前、年齢、住所、電話番号の変数を初期化するコンストラクタ
 	 */
 	public Person() {
-		name = null;
-		ages = null;
+		name = "";
+		ages = "";
 		age = 0;
-		address = null;
-		tel = null;
+		address = "";
+		tel = "";
 	}
 	/**
-	 * 名前を受け取り、出力するメソッド
+	 * 名前、年齢（String型）、住所、電話番号を変数に代入
 	 * @param name 人物名
-	 */
-	public void setName (String name) {
-		this.name = name;
-		System.out.println("私の名前は" + name + "です。");
-	}
-	/**
-	 * 年齢をString型で受け取り、出力するメソッド
 	 * @param ages 年齢（String型で入力された文字列）
-	 */
-	public void setAge (String ages) {
-		this.ages = ages;
-		System.out.println("私の名前は" + ages + "です。");
-	}
-	/**
-	 * 年齢をint型で受け取り、出力するメソッド
-	 * @param age 年齢（int型で入力された数値）
-	 */
-	public void setAge (int age) {
-		this.age = age;
-		System.out.println("私の名前は" + age + "です。");
-	}
-	/**
-	 * 住所を受け取り、出力するメソッド
 	 * @param address 住所
-	 */
-	public void setAddress (String address) {
-		this.address = address;
-		System.out.println("私の名前は" + address + "です。");
-	}
-	/**
-	 * 電話番号を受け取り、出力するメソッド
 	 * @param tel 電話番号
 	 */
-	public void setTel (String tel) {
+	public void setList(String name, String ages, String address, String tel) {
+		this.name = name;
+		this.ages = ages;
+		this.address = address;
 		this.tel = tel;
+
+		System.out.println("私の名前は" + name + "です。");
+		System.out.println("私の名前は" + ages + "です。");
+		System.out.println("私の名前は" + address + "です。");
+		System.out.println("私の名前は" + tel + "です。");
+	}
+	/**
+	 * 名前、年齢（int型）、住所、電話番号を変数に代入
+	 * @param name 人物名
+	 * @param ages 年齢（int型で入力された数値）
+	 * @param address 住所
+	 * @param tel 電話番号
+	 */
+	public void setList(String name, int age, String address, String tel) {
+		this.name = name;
+		this.age = age;
+		this.address = address;
+		this.tel = tel;
+
+		System.out.println("私の名前は" + name + "です。");
+		System.out.println("私の名前は" + age + "です。");
+		System.out.println("私の名前は" + address + "です。");
+		System.out.println("私の名前は" + tel + "です。");
+	}
+	/**
+	 * Listを出力
+	 */
+	public void show() {
+		System.out.println("私の名前は" + name + "です。");
+		System.out.println("私の名前は" + ages + "です。");
+		System.out.println("私の名前は" + address + "です。");
 		System.out.println("私の名前は" + tel + "です。");
 	}
 }
