cc1c850a5c2626e428d69502e38a88106f85affd
Haruki <117291500+Oharuno-heya@users.noreply.github.com>
2024-04-27 01:56:50 +0900

    Update Information.java

--- Information.java
+++ Information.java
@@ -7,11 +7,10 @@ public class Infomation {
 	public static void main(String[] args) {
 		//　個人情報リストの配列を定義
 		Person[] pers = new Person[3];
-		 for (int i = 0; i < pers.length; i++) {
-			 pers[i] = new Person();
-		 }
-		 
-		 //	個人情報リスト配列に要素を入力
+		for (int i = 0; i < pers.length; i++) {
+			pers[i] = new Person();
+		}
+		//	個人情報リスト配列に要素を入力
 		pers[0].setList("山田","23","東京都大田区","090‐0000-0000");
 		pers[1].setList("田中", 20, "埼玉県上尾市", "070‐1111-2222");
 		pers[2].setList("国光", 29, "山梨県笛吹市", "080‐4444-5555");
