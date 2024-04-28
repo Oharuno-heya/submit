06ef9327cc2bca177fafc8e3175198eeb75ffcc4
Haruki <117291500+Oharuno-heya@users.noreply.github.com>
2024-04-29 01:29:31 +0900

    Update Person.java

--- Person.java
+++ Person.java
@@ -1,4 +1,5 @@
-ackage jaavaskillcheckclassfunction;
+package jaavaskillcheckclassfunction;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -16,25 +17,27 @@ public class Person {
 	private	String address;
 	/** 電話番号の変数 */
 	private	String tel;
+	
 	/**
-	 * 名前、年齢、住所、電話番号の変数を初期化するコンストラクタ
+	 * 名前、年齢(String型)、住所、電話番号の変数を初期化するコンストラクタ
 	 */
-	public Person(String name, int ages, String address, String tel) {
+	public Person(String name, String ages, String address, String tel) {
+		this.name = name;
+		this.ages = ages;
+		this.address = address;
+		this.tel = tel;
+	}
+	
+	/**
+ 	 *　名前、年齢(int型)、住所、電話番号の変数を初期化するコンストラクタ
+   	 */
+	public Person(String name, int age, String address, String tel) {
 		this.name = name;
 		this.age = age;
 		this.address = address;
 		this.tel = tel;
 	}
-	/**
- 	 *
-   	 */
-	public void setAge(Object age) {
-		if (age instanceof String) {
-            		this.age = Integer.parseInt((String) age);
-       		} else if (age instanceof Integer) {
-            		this.age = (int) age;
-        	}
-    	}
+	
 	/**
 	 * Listを出力
 	 */
@@ -45,6 +48,7 @@ public class Person {
 		} else {
 			System.out.println("私の名前は" + age + "です。");
 		}
+
 		System.out.println("私の名前は" + address + "です。");
 		System.out.println("私の名前は" + tel + "です。");
 	}
