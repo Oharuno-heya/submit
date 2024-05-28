bf66fe6f750454e3f68205a727e217dab1065969
Haruki <117291500+Oharuno-heya@users.noreply.github.com>
2024-05-28 08:57:14 +0800

    ExceptionA.java を更新

--- src/javaskillcheck9/ExceptionA.java
+++ src/javaskillcheck9/ExceptionA.java
@@ -13,6 +13,7 @@ public class ExceptionA implements ExceptionInterface{
 	/**
 	 * 名前が10文字未満かつ年齢が140歳以下かを確認して、真偽値を返す
 	 * @return 名前が10文字未満かつ0歳以上140歳未満のとき、Trueを返す。
+  * @throws returnの条件以外の時、例外処理をする。
 	 */
 	public boolean check(String name, int age) throws CustomException{
 		this.name = name;
@@ -26,6 +27,7 @@ public class ExceptionA implements ExceptionInterface{
 	}
 	/**
 	 * 出力
+  * @throws 出力が正常に行われない時、例外処理をする
 	 */
 	public void run() throws CustomException{
 		if (System.out.checkError()) {
