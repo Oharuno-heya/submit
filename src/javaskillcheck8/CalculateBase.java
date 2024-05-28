ed094c0a51b055e486b18f175529be82ca64d765
Haruki <117291500+Oharuno-heya@users.noreply.github.com>
2024-05-28 18:10:30 +0800

    CalculateBase.java を更新

--- src/javaskillcheck8/CalculateBase.java
+++ src/javaskillcheck8/CalculateBase.java
@@ -3,11 +3,11 @@ package javaskillcheck8;
 /**
  * インターフェイス
  */
-public interface CalculateBase {
+public interface calculateBase {
 	/**
 	 * 抽象メソッド
 	 * @param num1　数値１
 	 * @param num2　数値2
 	 */
-	public void Calculate(int num1, int num2);
+	public void calculate(int num1, int num2);
 }
